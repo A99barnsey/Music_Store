7d33c14b61219d7cb99b45bb59093e44460efc0b
A99barnsey <33737199+A99barnsey@users.noreply.github.com>
2018-11-13 13:27:00 -0600

    Add project files.

---
+++ Music_Store/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!F:\OneDrive\Visual Studio 2017 Projects\Web\MVA_Music_Store\Music_Store\Music_Store\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
